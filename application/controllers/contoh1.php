3f3046fd34d133201a9895df1c83cd71607c471b
Fahlevvy <12220800@bsi.ac.id>
2023-09-27 18:56:04 +0700

    mengerjakan contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Ridho Fahlevy
+        Saya tinggal di bekasi
+        olahraga favorit saya bersepeda";
+    }
+}
